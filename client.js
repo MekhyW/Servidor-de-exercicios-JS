2d0207e28ddc0e69f35af698472aa7f265cde9e8
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-11 07:58:23 -0300

    Get token

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
 axios
-  .get("https://catfact.ninja/fact")
-  .then((response) => console.log(response.data.fact));
+    //.get("https://catfact.ninja/fact")
+    //.then((response) => console.log(response.data.fact));
+    .post("https://tecweb-js.insper-comp.com.br/token", {username: "USERNAME HERE"}, {headers: {"Content-Type": "application/json"}})
+    .then((response) => console.log(response.data.accessToken));

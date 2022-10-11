bad036ee50678aee4dc937a54eadde29847b5a38
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-11 10:44:47 -0300

    submitAnswers

--- client.js
+++ client.js
@@ -2,14 +2,21 @@ const axios = require("axios");
 fs = require('fs'); //native node.js module to read and write files
 var username = require('./username.json').username;
 var token;
+var questionSlugs = [];
+var questionParameters = [];
+var answers = [];
 
 function writeExercises(parsedData)
 {
+    questionSlugs = Object.keys(parsedData);
     var output = "";
     Object.entries(parsedData).forEach(([key, value]) => {
         output += '<h2>'+key+'</h2>\n<ul>\n';
         Object.entries(value).forEach(([key, subvalue]) => {
             output += '<li>'+key+': '+JSON.stringify(subvalue)+'</li>\n';
+            if (key == 'entrada') {
+                questionParameters = questionParameters.concat(JSON.parse(JSON.stringify(subvalue)));
+            }
         });
         output += '</ul>\n';
     });
@@ -36,10 +43,46 @@ async function getToken()
 async function getExercises() 
 {
     await getToken();
-    //console.log(token);
-    axios
+    await axios
         .get("https://tecweb-js.insper-comp.com.br/exercicio", {headers: {"Authorization": "Bearer "+token, "Content-Type": "application/json"}})
         .then((response) => writeExercises(JSON.parse(JSON.stringify(response.data))));
 }
 
-getExercises();
+async function submitAnswers(answers) {
+    for (i = 0; i < questionSlugs.length; i++)
+    {
+        console.log('Submitting answer for '+questionSlugs[i]);
+        promise = new Promise((resolve, reject) => {
+            axios
+                .post("https://tecweb-js.insper-comp.com.br/exercicio/"+questionSlugs[i], {"resposta": answers[i]}, {headers: {"Authorization": "Bearer "+token, "Content-Type": "application/json"}})
+                .then((response) => console.log(response.data));
+        });
+        await promise;
+    }
+}
+
+function soma(input) {
+    return parseInt(input.a) + parseInt(input.b);
+}
+
+function tamanhostring(input) {
+    return input.string.length;
+}
+
+function nomedousuario(input) {
+    return input.email.split('@')[0];
+}
+
+async function solve() {
+    answers = [soma(questionParameters[0]), 
+    tamanhostring(questionParameters[1]), 
+    nomedousuario(questionParameters[2])];
+}
+
+async function main() {
+    await getExercises();
+    console.log(questionParameters);
+    await solve();
+    await submitAnswers(answers);
+}
+main();

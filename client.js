abe98e9661003305d88f22e1f13417b0ed60cc37
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-11 09:34:10 -0300

    Get list of exercises and save to html

--- client.js
+++ client.js
@@ -1,6 +1,46 @@
 const axios = require("axios");
-axios
-    //.get("https://catfact.ninja/fact")
-    //.then((response) => console.log(response.data.fact));
-    .post("https://tecweb-js.insper-comp.com.br/token", {username: "USERNAME HERE"}, {headers: {"Content-Type": "application/json"}})
-    .then((response) => console.log(response.data.accessToken));
+fs = require('fs'); //native node.js module to read and write files
+var username = require('./username.json').username;
+var token;
+
+function writeExercises(parsedData)
+{
+    console.log(parsedData);
+    var output = "";
+    Object.entries(parsedData).forEach(([key, value]) => {
+        output += '<h2>'+key+'</h2>\n<ul>\n';
+        Object.entries(value).forEach(([key, subvalue]) => {
+            output += '<li>'+key+': '+subvalue+'</li>\n';
+        });
+        output += '</ul>\n';
+    });
+    fs.writeFile('exercises.html', output, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        else {
+            console.log(output + '-> exercises.html');
+        }
+    });
+}
+
+async function getToken() 
+{
+    promise = new Promise((resolve, reject) => {
+        axios
+            .post("https://tecweb-js.insper-comp.com.br/token", {username: username}, {headers: {"Content-Type": "application/json"}})
+            .then((response) => resolve(response.data.accessToken));
+    });
+    token = await promise;
+}
+
+async function getExercises() 
+{
+    await getToken();
+    //console.log(token);
+    axios
+        .get("https://tecweb-js.insper-comp.com.br/exercicio", {headers: {"Authorization": "Bearer "+token, "Content-Type": "application/json"}})
+        .then((response) => writeExercises(JSON.parse(JSON.stringify(response.data))));
+}
+
+getExercises();
